a0744d24cd22619be03bfdbed5e4d44b399ab3a4
Md Ahmed Khan <ahmed.khan@oneconvergence.com>
2023-03-03 12:04:16 +0530

    Create log.py

---
+++ log.py
@@ -0,0 +1,12 @@
+import urllib3
+#from mlflow import log_metric
+import logging
+import os
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    for i in range(10):
+        logging.warning('Logger Test')
+        print("Print Test")
+        
+        #log_metric ("train_loss", i*i, step=i)
